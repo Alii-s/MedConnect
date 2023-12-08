de12be8143e65d012d50f46ad789a25258daef1a
Alii-s <119129254+Alii-s@users.noreply.github.com>
2023-12-08 14:28:43 +0200

    Changed File

---
+++ DBapplication/Trigger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MedConnect
+{
+    public partial class Trigger : Form
+    {
+        public Trigger()
+        {
+            InitializeComponent();
+        }
+    }
+}
