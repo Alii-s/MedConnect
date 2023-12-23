ce458a49a44a6753eaeaa9b0351f6719088aca78
Alii-s <119129254+Alii-s@users.noreply.github.com>
2023-12-23 11:26:08 +0200

    Reverting Secretary

--- MedConnect/DBapplication/Program.cs
+++ MedConnect/DBapplication/Program.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
-using MedConnect.Secretary;
 
 namespace MedConnect
 {
@@ -16,7 +15,7 @@ namespace MedConnect
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Secretary.Secretary(11));
+            Application.Run(new Trigger());
         }
     }
 }
